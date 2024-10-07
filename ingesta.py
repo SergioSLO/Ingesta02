c4887e2c066eff6fa9471f9d46b14de5b3684ea5
Sergio Lezama <98142685+SergioSLO@users.noreply.github.com>
2024-10-06 23:28:55 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -2,15 +2,17 @@ import boto3
 import mysql.connector
 import csv
 
-db_host = "54.163.40.147"  
-db_user = "root"   
+db_host = "3.230.28.178"  
+db_user = "postgres"   
 db_password = "utec" 
-db_name = "tienda" 
-table_name = "fabricantes" 
-db_port = 8005 
+db_name = "rockie" 
+table_name1 = "activities" 
+table_name2 = "student" 
+db_port = 8002 
 
-ficheroUpload = "data.csv"
-nombreBucket = "sslo-output01"
+ficheroUpload1 = "activities.csv"
+ficheroUpload2 = "student.csv"
+nombreBucket = "bucket-ingesta-parcial"
 
 conn = mysql.connector.connect(
     host=db_host,
@@ -21,21 +23,31 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
-query = f"SELECT * FROM {table_name}"
-cursor.execute(query)
+query1 = f"SELECT * FROM {table_name1}"
+cursor.execute(query1)
+rows1 = cursor.fetchall()
+column_names1 = [i[0] for i in cursor.description]
 
-rows = cursor.fetchall()
-column_names = [i[0] for i in cursor.description]
-
-with open(ficheroUpload, mode='w', newline='') as file:
+with open(ficheroUpload1, mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(column_names)  
-    writer.writerows(rows) 
+    writer.writerow(column_names1)  
+    writer.writerows(rows1) 
+
+query2 = f"SELECT * FROM {table_name2}"
+cursor.execute(query2)
+rows2 = cursor.fetchall()
+column_names2 = [i[0] for i in cursor.description]
+
+with open(ficheroUpload2, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(column_names2)  
+    writer.writerows(rows2) 
 
 cursor.close()
 conn.close()
 
 s3 = boto3.client('s3')
-s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+s3.upload_file(ficheroUpload1, nombreBucket, ficheroUpload1)
+s3.upload_file(ficheroUpload2, nombreBucket, ficheroUpload2)
 
-print("Ingesta completada y archivo subido a S3")
+print("Ingesta completada y archivos subidos a S3")
