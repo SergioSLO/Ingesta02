22f2753cbd605184353884a9b78c872af7b2a02e
Sergio Lezama <98142685+SergioSLO@users.noreply.github.com>
2024-10-06 23:31:09 -0500

    Update and rename ingesta.py to ingesta-rockie.py

--- ingesta-rockie.py
+++ ingesta-rockie.py
@@ -3,17 +3,18 @@ import mysql.connector
 import csv
 
 db_host = "3.230.28.178"  
-db_user = "postgres"   
+db_user = "root"   
 db_password = "utec" 
-db_name = "rockie" 
-table_name1 = "activities" 
-table_name2 = "student" 
+db_name = "mysql" 
+table_name1 = "Rockie" 
+table_name2 = "Accesorio"
 db_port = 8002 
 
-ficheroUpload1 = "activities.csv"
-ficheroUpload2 = "student.csv"
+ficheroUpload1 = "rockie.csv"
+ficheroUpload2 = "accesorio.csv"
 nombreBucket = "bucket-ingesta-parcial"
 
+# Conexión a la base de datos
 conn = mysql.connector.connect(
     host=db_host,
     port=db_port, 
@@ -23,29 +24,35 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
+# Consulta para la tabla Rockie
 query1 = f"SELECT * FROM {table_name1}"
 cursor.execute(query1)
 rows1 = cursor.fetchall()
 column_names1 = [i[0] for i in cursor.description]
 
-with open(ficheroUpload1, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(column_names1)  
-    writer.writerows(rows1) 
+# Escribir los resultados de la tabla Rockie en un archivo CSV
+with open(ficheroUpload1, mode='w', newline='') as file1:
+    writer1 = csv.writer(file1)
+    writer1.writerow(column_names1)  
+    writer1.writerows(rows1) 
 
+# Consulta para la tabla Accesorio
 query2 = f"SELECT * FROM {table_name2}"
 cursor.execute(query2)
 rows2 = cursor.fetchall()
 column_names2 = [i[0] for i in cursor.description]
 
-with open(ficheroUpload2, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(column_names2)  
-    writer.writerows(rows2) 
+# Escribir los resultados de la tabla Accesorio en un archivo CSV
+with open(ficheroUpload2, mode='w', newline='') as file2:
+    writer2 = csv.writer(file2)
+    writer2.writerow(column_names2)  
+    writer2.writerows(rows2) 
 
+# Cerrar cursor y conexión
 cursor.close()
 conn.close()
 
+# Subir archivos a S3
 s3 = boto3.client('s3')
 s3.upload_file(ficheroUpload1, nombreBucket, ficheroUpload1)
 s3.upload_file(ficheroUpload2, nombreBucket, ficheroUpload2)
